3a45ec6a641c26dbe6fa2440b3ace9e190601d16
Tilman Potthof <tilmanpotthof@googlemail.com>
2016-01-03 23:39:06 +0100

    Basic dev tasks with watch and connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,42 @@ module.exports = function (grunt) {
                 sourceMap: true
             },
             dist: {
-                files: {
-                    "dist/app.js": "src/app.js"
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ['*.js'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        watch: {
+            scripts: {
+                options: {
+                    livereload: true
+                },
+                files: "src/**/*.js",
+                tasks: ["default"]
+            },
+            pages: {
+                options: {
+                    livereload: true
+                },
+                files: "index.html"
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    open: 'http://localhost:9001'
                 }
             }
         }
     });
 
+    grunt.registerTask("dev", ["default", "connect", "watch"]);
     grunt.registerTask("default", ["babel"]);
 
 };
